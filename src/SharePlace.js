c92f5efd90e83d87e258590678b6cef226e405f2
Erudite885 <erudite885@gmail.com>
2023-02-01 21:44:06 +0100

    writing the show handler

--- src/SharePlace.js
+++ src/SharePlace.js
@@ -1,9 +1,43 @@
-class PlaceFinder {
-    constructor(){
-        const addressForm = document.querySelector('form');
-        const locateUserBtn = document.getElementById('locate-btn');
+import { Modal } from "./UI/Modal";
 
-        locateUserBtn.addEventListener('click', )
+class PlaceFinder {
+  constructor() {
+    const addressForm = document.querySelector("form");
+    const locateUserBtn = document.getElementById("locate-btn");
+
+    locateUserBtn.addEventListener("click", this.locateUserHandler);
+    addressForm.addEventListener("submit", this.findAddressHandler);
+  }
+
+  locateUserHandler() {
+    if (!navigator.geolocation) {
+      alert(
+        "Location feature not available on your browser - please upgrade your browser"
+      );
+      return;
     }
-    locateUserHandler(){}
+    const modal = new Modal("loading-modal-content", "please wait");
+    modal.show();
+    navigator.geolocation.getCurrentPosition(
+      (successResult) => {
+        modal.hide();
+        console.log(successResult);
+        const coordinates = {
+          lat: successResult.coords.latitude + Math.random() * 50,
+          lng: successResult.coords.longitude + Math.random() * 50,
+        };
+        console.log(coordinates);
+      },
+      (error) => {
+        modal.hide();
+        alert(
+          "Could not locate position, please enter address manually and try again"
+        );
+      }
+    );
+  }
+
+  findAddressHandler() {}
 }
+
+const place = new PlaceFinder();
