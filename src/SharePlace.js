ac8f9c468b9b8cadb6ce4ffc556e7b5439e470d5
Erudite885 <erudite885@gmail.com>
2023-02-01 13:49:17 +0100

    src folder

---
+++ src/SharePlace.js
@@ -0,0 +1,9 @@
+class PlaceFinder {
+    constructor(){
+        const addressForm = document.querySelector('form');
+        const locateUserBtn = document.getElementById('locate-btn');
+
+        locateUserBtn.addEventListener('click', )
+    }
+    locateUserHandler(){}
+}
