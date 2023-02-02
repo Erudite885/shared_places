dbd63dce6624454391becd148129a2bb97d88a6e
Erudite885 <erudite885@gmail.com>
2023-02-02 14:13:11 +0100

    rendering coordinates

--- src/UI/Map.js
+++ src/UI/Map.js
@@ -1,22 +1,22 @@
 export class Map {
   constructor(coords) {
-    this.coordinates = coords;
-    this.render();
+    // this.coordinates = coords;
+    this.render(coords);
   }
-  render() {
+  render(coordinates) {
     if (!google) {
       alert("Could not load maps - Please try again later.");
       return;
     }
 
     const map = new google.maps.Map(document.getElementById("map"), {
-      center: this.coordinates,
+      center: coordinates,
       zoom: 16,
     });
 
     new google.maps.Marker({
-        position: this.coordinates,
-        map: map
+      position: coordinates,
+      map: map,
     });
   }
 }
