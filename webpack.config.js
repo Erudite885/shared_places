c8ed25ec8516762fd6265e2f9931cba22695f072
Erudite885 <erudite885@gmail.com>
2023-02-04 08:30:11 +0100

    config & json files

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const CleanPlugin = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    SharePlace: "./src/SharePlace.js",
+    MyPlace: "./src/MyPlace.js",
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist", "assets", "scripts"),
+    publicPath: "/assets/scripts/",
+    // publicPath should start with /
+    // hashFunction: "xxhash64",
+  },
+  devtool: "cheap-module-source-map",
+  devServer: {
+    // contentBase:  "./dist/"
+    static: "./dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              [
+                "@babel/preset-env",
+                { useBuiltIns: "usage", corejs: { version: 3 } },
+              ],
+            ],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [new CleanPlugin.CleanWebpackPlugin()],
+};
